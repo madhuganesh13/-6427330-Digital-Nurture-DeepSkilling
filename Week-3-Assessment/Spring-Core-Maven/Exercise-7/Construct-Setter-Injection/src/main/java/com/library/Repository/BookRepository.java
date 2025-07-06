ab1bf4378114ee5c8cc8926c576025d8494144a3
Madhu Ganesun R <160342606+madhuganesh13@users.noreply.github.com>
2025-07-07 00:42:25 +0530

    Delete Week-3-Assessment/Spring-Core-Maven directory

--- Week-3-Assessment/Spring-Core-Maven/Exercise-7/Construct-Setter-Injection/src/main/java/com/library/Repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.Repository;
-
-public class BookRepository {
-    public void save(String bookName) {
-        System.out.println("BookRepository: Saving book → " + bookName);
-    }
-}
